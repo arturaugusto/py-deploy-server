23e91dabe15dcfdf610cfaa2e9afe36040a1b544
Artur <arturaugusto@gmail.com>
2018-06-05 09:13:51 -0300

    Update 01_pre_configure_run_as_root.sh

--- 01_pre_configure_run_as_root.sh
+++ 01_pre_configure_run_as_root.sh
@@ -3,4 +3,4 @@ add-apt-repository ppa:vbernat/haproxy-1.6
 
 sudo apt-get update
 
-sudo apt-get install certbot haproxy apache2 python3 python3-dev python python-dev  libapache2-mod-proxy-uwsgi libapache2-mod-uwsgi pwgen python-pip python3-pip
+sudo apt-get install certbot haproxy apache2 python3 python3-dev python python-dev libapache2-mod-proxy-uwsgi libapache2-mod-uwsgi pwgen python-pip python3-pip python3-virtualenv virtualenv
